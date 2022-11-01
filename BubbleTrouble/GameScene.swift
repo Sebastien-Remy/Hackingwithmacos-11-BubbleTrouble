09eeccd36d8e7cb3c443f168a9608a8f456a66fb
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-01 13:09:12 +0100

    add physics

--- BubbleTrouble/GameScene.swift
+++ BubbleTrouble/GameScene.swift
@@ -23,6 +23,10 @@ class GameScene: SKScene {
          bubbleTextures.append(SKTexture(imageNamed: "bubblePurple"))
          bubbleTextures.append(SKTexture(imageNamed: "bubbleRed"))
          
+         // pysics
+         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+         physicsWorld.gravity = CGVector.zero
+         
          // Create bubbles
          for _ in 1...8 {
              createBubble()
@@ -59,5 +63,52 @@ class GameScene: SKScene {
         let xPos = Int.random(in: 0..<800)
         let yPos = Int.random(in: 0..<600)
         bubble.position = CGPoint(x: xPos, y: yPos)
+        
+        // physics
+        configurePhysics(for: bubble)
+        
+        // Next
+        nextBubble()
+    }
+    
+    
+    
+    func nextBubble() {
+        // move on the next bubble texture
+        currentBubbleTexture += 1
+        
+        // if we've use all the textures start at the begining
+        if currentBubbleTexture == bubbleTextures.count {
+            currentBubbleTexture = 0
+        }
+        
+        // add a random number between 1 & 3 to maximumNumber
+        maximumNumber += Int.random(in: 1...3)
+        
+        // fix the mystery problem (don't use 6 and 9 beacause they are similar when the turn
+        let strMaximumNumber = String(maximumNumber)
+        
+        if strMaximumNumber.last! == "6" {
+            maximumNumber += 1
+        }
+        
+        if strMaximumNumber.last! == "9" {
+            maximumNumber += 1
+        }
+    }
+    
+    func configurePhysics(for bubble: SKSpriteNode) {
+        bubble.physicsBody = SKPhysicsBody(circleOfRadius: bubble.size.width / 2)
+        bubble.physicsBody?.linearDamping = 0.0
+        bubble.physicsBody?.angularDamping = 0.0
+        bubble.physicsBody?.restitution = 1.0
+        bubble.physicsBody?.friction = 0.0
+        
+        let motionX = Double.random(in: -200...200)
+        let motionY = Double.random(in: -200...200)
+        
+        bubble.physicsBody?.velocity = CGVector(dx: motionX, dy: motionY)
+        bubble.physicsBody?.angularVelocity = Double.random(in: 0...1)
+        
     }
 }
