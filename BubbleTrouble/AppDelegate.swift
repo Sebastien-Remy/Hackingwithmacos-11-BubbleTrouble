e68dc56c9d910b987084ed4b9f05366c43df753d
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-01 12:35:15 +0100

    Initial Commit

---
+++ BubbleTrouble/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  BubbleTrouble
+//
+//  Created by Sebastien REMY on 01/11/2022.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ BubbleTrouble/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  BubbleTrouble
+//
+//  Created by Sebastien REMY on 01/11/2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+     override func didMove(to view: SKView) {
+    }
+}
